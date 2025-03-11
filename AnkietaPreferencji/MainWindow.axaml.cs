5c709b8abf3b50e1135eb90d895802713967776e
Szymon955 <gutek.gutomski@gmail.com>
2025-03-11 15:31:01 +0100

    zaminy

--- AnkietaPreferencji/MainWindow.axaml.cs
+++ AnkietaPreferencji/MainWindow.axaml.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
 
 namespace AnkietaPreferencji;
 
 public partial class MainWindow : Window
 {
+    private List<Surve>
     public MainWindow()
     {
         InitializeComponent();
